9172f07037d0e17e6e09c0e231d20800a3506385
epy0n0ff <epy0n.0ff@gmail.com>
2015-09-25 18:54:08 +0900

    Change observeOn(...) to registerSchedulersHook.

--- app/src/test/java/com/epy0n0ff/ci/RxTest.java
+++ app/src/test/java/com/epy0n0ff/ci/RxTest.java
@@ -1,11 +1,15 @@
 package com.epy0n0ff.ci;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
+import rx.android.plugins.RxAndroidPlugins;
+import rx.android.plugins.RxAndroidSchedulersHook;
 import rx.observers.TestSubscriber;
 import rx.schedulers.Schedulers;
 
@@ -19,6 +23,14 @@ import static org.junit.Assert.assertThat;
 @RunWith(RobolectricGradleTestRunner.class) @Config(
     sdk = 21,
     constants = BuildConfig.class) public class RxTest {
+  @Before public void setUp() {
+    RxAndroidPlugins.getInstance().registerSchedulersHook(new RxAndroidSchedulersHook() {
+      @Override public Scheduler getMainThreadScheduler() {
+        return Schedulers.newThread();
+      }
+    });
+  }
+
   @Test public void asyncTest() {
     Observable<Object> testObservable = Observable.create(new Observable.OnSubscribe<Object>() {
       @Override public void call(final Subscriber<? super Object> subscriber) {
@@ -30,7 +42,7 @@ import static org.junit.Assert.assertThat;
           subscriber.onError(e);
         }
       }
-    }).observeOn(Schedulers.newThread());
+    });
 
     TestSubscriber<Object> testSubscriber = new TestSubscriber<>();
     testObservable.subscribe(testSubscriber);
